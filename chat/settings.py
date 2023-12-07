2d9d7e0fb0d4196f27f87a56ce28cad9f694d365
dhruv-goyal-10 <dungeoninja@gmail.com>
2023-12-07 21:43:11 +0000

    Final changes for deployment with daphne and uvicorn

--- chat/settings.py
+++ chat/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "channels",
-    # "daphne",
+    # "channels",
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
